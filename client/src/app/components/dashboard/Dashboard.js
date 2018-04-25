73e249cb86c57d49c624b258e2395fe77487f19d
HP <andresfelipe9619@gmail.com>
2018-04-25 17:39:07 -0500

    Added router to dashboard

--- client/src/app/components/dashboard/Dashboard.js
+++ client/src/app/components/dashboard/Dashboard.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react';
 import {MenuDashboard} from './Menu.js';
-import {UsersTable} from './users/UsersTable';
+import {UserTable} from './users/UsersTable';
+import {ProductsTable} from './products/DevolutionsTable';
+import {OrdersTable} from './orders/DevolutionsTable';
+import {InvoicesTable} from './invoices/DevolutionsTable';
+import {DevolutionsTable} from './devolutions/DevolutionsTable';
 import {Grid} from 'semantic-ui-react'
+import {Route, Switch} from 'react-router-dom';
 export class Dashboard extends Component {
     render() {
         return (
@@ -14,6 +19,13 @@ export class Dashboard extends Component {
                         <MenuDashboard/>
                     </Grid.Column>
                     <Grid.Column width={12}>
+                        <Switch>
+                            <Route exact path="/usuarios" component={UserTable}/>
+                            <Route exact path="/productos" component={ProductsTable}/>
+                            <Route exact path="/pedidos" component={OrdersTable}/>
+                            <Route exact path="/facturas" component={InvoicesTable}/>
+                            <Route exact path="/devoluciones" component={DevolutionsTable}/>
+                        </Switch>
                         <UsersTable/>
                     </Grid.Column>
 
